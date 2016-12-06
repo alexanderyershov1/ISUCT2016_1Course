a85a792800409865f9eb76930ab6674249ff059d
Gritish765 <kropotina2108@mail.ru>
2016-12-06 09:47:00 +0400

    added classes

---
+++ src/LG.java
@@ -0,0 +1,33 @@
+/**
+ * Created by stud_6 on 6.12.16.
+ */
+public class LG extends Phone implements Android {
+    @Override
+    public void restart() {
+
+    }
+
+    @Override
+    public void write() {
+
+    }
+
+    public void call() {
+        System.out.println("Dziiin");
+    }
+
+//    @Override
+//    public void takepictures() {
+//
+//    }
+//
+//    @Override
+//    public void makeavideo() {
+//
+//    }
+//
+//    @Override
+//    public void () {
+//
+//    }
+}
