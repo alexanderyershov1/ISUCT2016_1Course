c3c1d65ff3544afd9777e6322b3c6213d643674a
anaitya <an-it-ka@mail.ru>
2016-12-19 13:21:52 +0400

    12

---
+++ src/Calculator.java
@@ -0,0 +1,9 @@
+/**
+ * Created by stud_6 on 19.12.16.
+ */
+public class Calculator {
+    public void calc(double a, double b,double x){
+        a = Math.pow(a,x);
+        b = Math.pow(b,x)
+    }
+}
