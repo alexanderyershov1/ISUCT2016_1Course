c2bc0e0a63c3e9f0caa3a901d1e51440943b1695
KN <KN@isuct.ru>
2017-06-01 22:15:23 +0300

    gui

--- src/Phone.java
+++ src/Phone.java
@@ -1,108 +1,108 @@
-<<<<<<< HEAD
-/**
- * Created by stud_6 on 17.10.16.
- */
-public class Phone  {
-    private int brand;
-    private String model;
-    private String size;
-
-//    @Override
-    public void call() {
-        System.out.println("Dzin");
-    }
-
-    public int getBrand() {
-        return brand;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public String getSize() {
-        return size;
-    }
-
-    public void setBrand(int brand) {
-
-        brand = brand;
-    }
-
-    public void setSize(String size) {
-        this.size = size;
-    }
-
-//    @Override
-//    void restart() {
+//<<<<<<< HEAD
+///**
+// * Created by stud_6 on 17.10.16.
+// */
+//public class Phone  {
+//    private int brand;
+//    private String model;
+//    private String size;
 //
+////    @Override
+//    public void call() {
+//        System.out.println("Dzin");
 //    }
-//    @Override
-//    void write() {
-//        System.out.println("sms");
-//    }
-
-    void v() {
-        System.out.println(brand);}
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-
-
-=======
-/**
- * Created by stud_6 on 17.10.16.
- */
-public class Phone  {
-    private int brand;
-    private String model;
-    private String size;
-
-//    @Override
-    public void call() {
-        System.out.println("Dzin");
-    }
-
-    public int getBrand() {
-        return brand;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public String getSize() {
-        return size;
-    }
-
-    public void setBrand(int brand) {
-
-        brand = brand;
-    }
-
-    public void setSize(String size) {
-        this.size = size;
-    }
-
-//    @Override
-//    void restart() {
 //
+//    public int getBrand() {
+//        return brand;
 //    }
-//    @Override
-//    void write() {
-//        System.out.println("sms");
+//
+//    public String getModel() {
+//        return model;
 //    }
-
-    void v() {
-        System.out.println(brand);}
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-
-
->>>>>>> a85a792800409865f9eb76930ab6674249ff059d
-}
+//
+//    public String getSize() {
+//        return size;
+//    }
+//
+//    public void setBrand(int brand) {
+//
+//        brand = brand;
+//    }
+//
+//    public void setSize(String size) {
+//        this.size = size;
+//    }
+//
+////    @Override
+////    void restart() {
+////
+////    }
+////    @Override
+////    void write() {
+////        System.out.println("sms");
+////    }
+//
+//    void v() {
+//        System.out.println(brand);}
+//
+//    public void setModel(String model) {
+//        this.model = model;
+//    }
+//
+//
+//
+//=======
+///**
+// * Created by stud_6 on 17.10.16.
+// */
+//public class Phone  {
+//    private int brand;
+//    private String model;
+//    private String size;
+//
+////    @Override
+//    public void call() {
+//        System.out.println("Dzin");
+//    }
+//
+//    public int getBrand() {
+//        return brand;
+//    }
+//
+//    public String getModel() {
+//        return model;
+//    }
+//
+//    public String getSize() {
+//        return size;
+//    }
+//
+//    public void setBrand(int brand) {
+//
+//        brand = brand;
+//    }
+//
+//    public void setSize(String size) {
+//        this.size = size;
+//    }
+//
+////    @Override
+////    void restart() {
+////
+////    }
+////    @Override
+////    void write() {
+////        System.out.println("sms");
+////    }
+//
+//    void v() {
+//        System.out.println(brand);}
+//
+//    public void setModel(String model) {
+//        this.model = model;
+//    }
+//
+//
+//
+//>>>>>>> a85a792800409865f9eb76930ab6674249ff059d
+//}
